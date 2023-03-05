9fab962493058ecb711b84ce1424c2b6c0caf317
GremlinOnTheComputer <societygremlin@gmail.com>
2023-03-05 12:18:03 -0600

    changed title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>{"dr. mexico's lair"}</title>
+        <title>{"brad's lair"}</title>
         <link rel="icon" href="favicon.gif" type="image/gif" />
         {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
